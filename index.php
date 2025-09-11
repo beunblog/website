0562af3d325dd614bde01f4d50afaf1ed3d97319
sarah <s.onrust@student.utwente.nl>
2025-09-11 22:12:25 +0200

    Correct db references

--- index.php
+++ index.php
@@ -6,28 +6,30 @@ $result = $db->query($sql);
 ?>
 <div class="container-fluid pt-5" id="post-list">
     <?php 
-    for ($x = 0; $x < $result->num_rows; $x++){
+
+       while($row = $result->fetch_assoc()) {
     echo '<div class="post-preview row item pt-5">';
     echo '<div id="image" class="col-2">
-            <img src=$img alt=$title style="height: 250px;">
+            <img src=' . $row["img_src"] .'alt=' . $row["title"] . ' style="height: 250px;">
           </div>
           <div id="text" class="col-8">
             <div id="title">
-              <h1 id="title_text">$title</h1>
+              <h1 id="title_text">' . $row["title"] . '</h1>
             </div>
             <div id="summary">
-              <p id="summary_text">$summary</p>
+              <p id="summary_text">'.$row["summary"].'</p>
             </div>
           </div>
           <div id="info" class="col-2">
             <div id="date" class="row" style="text-align:center;">
-              <b id="date_text">$date</b>
+              <b id="date_text">'.$row["date"].'</b>
             </div>
             <div id="button" class="row" style="margin-top:100%;">
               <a href="blog.html?post=$id class="btn btn-danger" role="button" aria-disabled="true">Read More</a>
             </div>
           </div>';
-    }
+    
+  }
     echo "</div>";
     ?>
   </div>
